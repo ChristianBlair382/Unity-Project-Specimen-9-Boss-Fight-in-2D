6ff32c96b1af1d026580b43fb50fd53f02042faf
Christian Blair <c.blair@marquette.edu>
2026-01-05 19:48:58 -0500

    VolleyOrb functionality implemented
    -The Volley Orb script is working as intended
    -Next implementation: Minion and Hazard scripts

---
+++ .history/Assets/Scripts/InGame Objects/VolleyOrb_20260105182312.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum orbState
+{
+    Shot = 0,
+    Reflected = 1
+}
+
+public class VolleyOrb : MonoBehaviour
+{
+    private orbState currentState = orbState.Shot;
+    private Rigidbody2D rb;
+    public float 
+        movementSpeed,
+        lifetime;
+    private GameObject player;
+    private Transform playerPosition;
+    
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerPosition = player.transform;
+        Vector2 direction = (playerPosition.position - transform.position).normalized;
+        rb.AddForce(direction * movementSpeed, ForceMode2D.Impulse);
+    }
+
+    void Update()
+    {
+        lifetime -= Time.deltaTime;
+        if(lifetime <= 0.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void 
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player" && currentState == orbState.Shot)
+        {
+            other.GetComponent<Player>().DamagePlayer(10);
+        } else if (other.tag == "PlayerAttackHitBox" && currentState == orbState.Shot)
+        {
+            currentState = orbState.Reflected;
+            rb.velocity = Vector2.zero;
+            Vector2 direction = (other.transform.position - transform.position).normalized;
+            rb.AddForce(-direction * movementSpeed, ForceMode2D.Impulse);
+        } else if (other.tag == "Enemy" && currentState == orbState.Reflected)
+        {
+            other.GetComponent<Specimen_9>().Stun();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ .history/Assets/Scripts/Meta Objects/Game_20260105174134.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public enum debugMode { ON, OFF }
+
+public enum gameState { PLAYING, PAUSED, OVER }
+
+public class Game : MonoBehaviour
+{
+    //STATISTICS
+    private debugMode debug = debugMode.OFF;
+    private gameState state;
+    private float playerSpawnX, playerSpawnY;
+    public float timer = 0.0f;
+    public int hitsTaken = 0;
+
+    //META OBJECTS
+    public SceneManager SM;
+    public CameraController CC;
+    public PlayerHealthManager PHM;
+    public TimerRenderer TR;
+    public GameObject playerCharacterPrefab;
+    public GameObject playerCharacter;
+    private Player playerScript;
+    [SerializeField] private GameObject pauseScreen;
+
+    void Start()
+    {
+        CC = GameObject.Find("Main_Camera").GetComponent<CameraController>();
+
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if(debug == debugMode.OFF) { debug = debugMode.ON; }
+            else { debug = debugMode.OFF; }
+        }
+
+        if(Inpu)
+
+        if(state == gameState.PLAYING)
+        {
+            timer += Time.deltaTime;
+        }
+    }
+}
