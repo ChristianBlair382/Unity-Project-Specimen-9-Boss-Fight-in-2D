c360f96d42d19ba6ff265279513888ff549a9f15
Christian Blair <c.blair@marquette.edu>
2026-07-31 16:06:32 -0400

    Title Screen Glowup
    - Title Screen now features a scrolling background with a portrait of Specimen 9. The Credits screen has been overhauled and completed.

--- Assets/Scripts/Meta Objects/StartMenuManager.cs
+++ Assets/Scripts/Meta Objects/StartMenuManager.cs
@@ -13,17 +13,42 @@ public class StartMenuManager : MonoBehaviour
         quitButton,
         creditsButton,
         backButton;
+    public Image 
+        Specimen9Portrait1,
+        Specimen9Portrait2,
+        Specimen9Eye1,
+        Specimen9Eye2;
     public SceneController sceneController;
+    public GameObject TTDOverlay;
+    private Rigidbody2D TTDOverlayRigidbody;
     void Awake()
     {
+        TTDOverlayRigidbody = TTDOverlay.GetComponent<Rigidbody2D>();
         startMenuCanvas.enabled = false;
         creditsCanvas.enabled = false;
+        Specimen9Portrait1.enabled = true;
+        Specimen9Portrait2.enabled = false;
+        Specimen9Eye1.enabled = false;
+        Specimen9Eye2.enabled = false;
         StartCoroutine(StartAnimation());
+    }
+    void Update()
+    {
+        if(TTDOverlay.transform.position.y < -6.9f)
+        {
+            TTDOverlay.transform.position = new Vector3(TTDOverlay.transform.position.x, 6.9f, TTDOverlay.transform.position.z);
+        }
+        TTDOverlayRigidbody.velocity = new Vector2(0f, -1f);
     }
     public void OnStartButton ()
     {
         startMenuCanvas.enabled = false;
         creditsCanvas.enabled = false;
+        // Switch the portrait to Specimen 9's portrait before loading the next scene
+        Specimen9Portrait1.enabled = false;
+        Specimen9Portrait2.enabled = true;
+        Specimen9Eye1.enabled = true;
+        Specimen9Eye2.enabled = true;
         sceneController.LoadNextScene();
     }
     public void OnCreditsButton ()
